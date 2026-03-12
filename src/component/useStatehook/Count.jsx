1a6e1d08cec3e60395538e9dd4ae275301c22a04
md shaddat shorav seyam <mdshaddatshoravseyam@gmail.com>
2026-03-12 06:19:29 +0600

    full count component

--- src/component/useStatehook/Count.jsx
+++ src/component/useStatehook/Count.jsx
@@ -4,13 +4,13 @@ export function Count() {
   const [c, setC] = useState(0);
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center bg-amber-600">
+    <div className="h-screen w-screen flex flex-col justify-center items-center bg-[#F6FBEA]">
       <h1 className="text-9xl">{c}</h1>
       <div className="flex flex-row gap-6 mt-7  ">
         
-        <button onClick={()=>setC(c-1)} className="cursor-pointer text-2xl border-b-emerald-600 bg-emerald-500 rounded-4xl p-3 hover:bg-purple-600">Decrement</button>
-        <button onClick={()=>setC(0)} className="cursor-pointer">Reset</button>
-        <button onClick={()=>setC(c+1)} className="cursor-pointer">Increment</button>
+        <button onClick={()=>setC(c-1)} className="cursor-pointer text-2xl border-b-emerald-600 bg-[#DCEFA9] rounded-2xl p-3 hover:bg-[#CBE77E] hover:scale-110 ">Decrement</button>
+        <button onClick={()=>setC(0)} className="cursor-pointer  text-2xl border-b-emerald-600 bg-[#DCEFA9] rounded-2xl p-3 hover:bg-[#CBE77E] hover:scale-110 ">Reset</button>
+        <button onClick={()=>setC(c+1)} className="cursor-pointer  text-2xl border-b-emerald-600 bg-[#DCEFA9] rounded-2xl p-3 hover:bg-[#CBE77E] hover:scale-110 ">Increment</button>
       </div>
     </div>
   );
